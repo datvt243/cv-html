e86ff713c8e5be4638fb283da76cbfb46a5ef185
Datto <votan.it@gmail.com>
2020-09-18 05:46:44 +0700

    add vue.config.js

--- portfolio/vue-todolist/vue.config.js
+++ portfolio/vue-todolist/vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/datvt243.github.io/' // Thay tên repository của các bạn vào đây nhé
+    ? '/datvt243/' // Thay tên repository của các bạn vào đây nhé
     : '/'
 }
